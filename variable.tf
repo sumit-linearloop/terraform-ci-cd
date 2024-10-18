7cb508394aaccad7c10d1b72e537f9d504a1682f
sumit-linearloop <sumit@linearloop.io>
2024-10-18 17:48:08 +0530

    main

--- variable.tf
+++ variable.tf
@@ -6,9 +6,7 @@ variable "do_token" {
 variable "ssh_public_key_path" {
   description = "Path to the SSH public key file"
   type        = string
-  default     = "D:/ssh/sumit.pub"  # Adjust this if necessary
 }
-
 
 variable "ssh_private_key_path" {
   description = "Path to the SSH private key"
@@ -18,37 +16,26 @@ variable "ssh_private_key_path" {
 variable "droplet_image" {
   description = "Droplet image to use"
   type        = string
-  default     = "ubuntu-20-04-x64"
 }
 
 variable "droplet_name" {
   description = "Name of the droplet"
   type        = string
-  default     = "web-server"
 }
 
 variable "droplet_region" {
   description = "Region for the droplet"
   type        = string
-  default     = "nyc3"
 }
 
 variable "droplet_size" {
   description = "Size of the droplet"
   type        = string
-  default     = "s-1vcpu-1gb"
 }
-
-
-# variable "github_repo" {
-#   description = "GitHub repository URL"
-#   type        = string
-# }
 
 variable "node_version" {
   description = "Node.js version to install"
   type        = string
-  default     = "14"
 }
 
 variable "dragonfly_repo" {
@@ -60,46 +47,46 @@ variable "firefly_repo" {
   description = "GitHub repository URL for Firefly"
   type        = string
 }
+
 variable "app_action" {
   description = "Action to perform on the app (setup_dragonfly, setup_firefly, destroy_dragonfly, destroy_firefly)"
   type        = string
-  default     = ""
 }
 
 variable "firefly_port" {
-    description = "port for firefly application"
-    type = number
+  description = "Port for Firefly application"
+  type        = number
 }
 
 variable "dragonfly_port" {
-    description = "port for dragonfly application"
-    type = number
+  description = "Port for Dragonfly application"
+  type        = number
 }
 
 variable "dragonfly_domain" {
-    description = "Dragonfly Domain"
-    type = string  
+  description = "Dragonfly Domain"
+  type        = string  
 }
 
 variable "firefly_domain" {
-    description = "Firefly Domain"
-    type = string
+  description = "Firefly Domain"
+  type        = string
 }
 
 variable "firefly_secret" {
-    description = "Secret name for Firefly"
+  description = "Secret name for Firefly"
 }
 
 variable "dragonfly_secret" {
-    description = "Secret name for Dragonfly"
+  description = "Secret name for Dragonfly"
 }
 
 variable "firefly_branch" {
-    description = "Branch for Firefly"
+  description = "Branch for Firefly"
 }
 
 variable "dragonfly_branch" {
-    description = "Branch for Dragon"
+  description = "Branch for Dragon"
 }
 
 variable "aws_access_key" {
@@ -117,8 +104,12 @@ variable "aws_region" {
   type        = string
 }
 
-# Github Token
 variable "github_token" {
   description = "GitHub token for authentication"
+  type        = string
+}
+
+variable "github_secret" {
+  description = "GitHub secret for additional operations"
   type        = string
 }
