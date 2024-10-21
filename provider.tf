d4c3aea0ab0c2987466fb16e507bf96d180261fc
sumit-linearloop <sumit@linearloop.io>
2024-10-21 17:01:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,20 +2,26 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.43.0"
+      version = "~> 2.43"  # Keep using 2.x series
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.72"  # This will still keep you within the 5.x range
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"   # Update if necessary
     }
   }
 }
+
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
