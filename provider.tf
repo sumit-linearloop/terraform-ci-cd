3ad53159765916729764fd53cec7425b4a5789bc
sumit-linearloop <sumit@linearloop.io>
2024-10-21 11:09:03 +0530

    12345

--- provider.tf
+++ provider.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "2.43.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
@@ -10,4 +14,10 @@ terraform {
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
--- variable.tf
+++ variable.tf
@@ -100,14 +100,14 @@ variable "dragonfly_branch" {
     description = "Branch for Dragon"
 }
 
+# Variable declarations
 variable "aws_access_key" {
   description = "AWS Access Key"
   type        = string
 }
 
-
 variable "aws_secret_key" {
-  description = "AWS secret key"
+  description = "AWS Secret Key"
   type        = string
 }
 
